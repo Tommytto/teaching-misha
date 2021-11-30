9e89ab1e4219fa6810e4bb4ab91714a317e0ba0c
Timur Togyzbaev <timur.togyzbaev@instamart.ru>
2021-11-30 20:29:05 +0200

    [task], http

---
+++ http/README.md
@@ -0,0 +1,1 @@
+(MDN Http session)[https://developer.mozilla.org/en-US/docs/Web/HTTP/Session]
---
+++ http/index.js
@@ -0,0 +1,30 @@
+const net = require('net');
+
+const server = net.createServer();
+
+server.on('connection', handleConnection)
+server.listen(9000, function() {
+    console.log('server listening to %j', server.address());
+});
+function handleConnection(conn) {
+    var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
+    console.log('new client connection from %s', remoteAddress);
+    conn.on('data', onConnData);
+    conn.once('close', onConnClose);
+    conn.on('error', onConnError);
+    function onConnData(d) {
+        console.log('connection data from %s: %j', remoteAddress, d);
+        conn.write(d);
+    }
+    function onConnClose() {
+        console.log('connection from %s closed', remoteAddress);
+    }
+    function onConnError(err) {
+        console.log('Connection %s error: %s', remoteAddress, err.message);
+    }
+
+    conn.write("HTTP/1.1 200 OK\n" +
+        "\n" +
+        "<h1>hello world</h1>");
+    conn.close();
+}

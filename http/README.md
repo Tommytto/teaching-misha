23c06b942e294b39a442e7c584acfbaa2d422860
Timur Togyzbaev <timur.togyzbaev@instamart.ru>
2021-12-01 07:56:29 +0200

    lesson finished

--- http/README.md
+++ http/README.md
@@ -1,14 +1,7 @@
 ## Materials
-(MDN Http session)[https://developer.mozilla.org/en-US/docs/Web/HTTP/Session]
+(MDN Http session)[https://developer.mozilla.org/en-US/docs/Web/HTTP/Session] 
 
 ## Task
-Write class `MishaServer` with interface 
-```javascript
-class MishaServer {
-    getWeather() {};
-}
-```
-Method `getWeather` returns weather for Moscow.
+Method `getJoke` returns random joke.
 
-For getting weather info you have to use public API, for example
-https://rapidapi.com/community/api/open-weather-map/
+https://api.chucknorris.io/jokes/random
--- http/index.js
+++ http/index.js
@@ -2,29 +2,42 @@ const net = require('net');
 
 const server = net.createServer();
 
-server.on('connection', handleConnection)
-server.listen(9000, function() {
-    console.log('server listening to %j', server.address());
-});
-function handleConnection(conn) {
-    var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
-    console.log('new client connection from %s', remoteAddress);
-    conn.on('data', onConnData);
-    conn.once('close', onConnClose);
-    conn.on('error', onConnError);
-    function onConnData(d) {
-        console.log('connection data from %s: %j', remoteAddress, d);
-        conn.write(d);
-    }
-    function onConnClose() {
-        console.log('connection from %s closed', remoteAddress);
-    }
-    function onConnError(err) {
-        console.log('Connection %s error: %s', remoteAddress, err.message);
-    }
+server.on('connection', handleConnection);
 
-    conn.write("HTTP/1.1 200 OK\n" +
+server.listen(9000, function() {
+    console.log('TCP server started on localhost:9000');
+})
+
+function handleConnection(connection) {
+    const remoteAddress = connection.remoteAddress + ':' + connection.remotePort;
+    console.log('new client connection from %s', remoteAddress);
+
+    connection.write("HTTP/1.1 200 OK\n" +
+        "Content-Type: text/html; charset=utf-8\n" +
+        "Content-Length: 55743\n" +
+        "Connection: keep-alive\n" +
+        "Cache-Control: s-maxage=300, public, max-age=0\n" +
+        "Content-Language: en-US\n" +
+        "Date: Thu, 06 Dec 2018 17:37:18 GMT\n" +
+        "ETag: \"2e77ad1dc6ab0b53a2996dfd4653c1c3\"\n" +
+        "Server: meinheld/0.6.1\n" +
+        "Strict-Transport-Security: max-age=63072000\n" +
+        "X-Content-Type-Options: nosniff\n" +
+        "X-Frame-Options: DENY\n" +
+        "X-XSS-Protection: 1; mode=block\n" +
+        "Vary: Accept-Encoding,Cookie\n" +
+        "Age: 7\n" +
         "\n" +
-        "<h1>hello world</h1>");
-    conn.close();
+        "<!DOCTYPE html>\n" +
+        "<html lang=\"en\">\n" +
+        "<head>\n" +
+        "  <meta charset=\"utf-8\">\n" +
+        "  <title>A simple webpage</title>\n" +
+        "</head>\n" +
+        "<body>\n" +
+        "  <h1>Simple HTML5 webpage</h1>\n" +
+        "  <p>Hello, world!</p>\n" +
+        "</body>\n" +
+        "</html>")
 }
+
